e38a3e1e7ceba48980026e27c626784b0237963c
민경민 <tom.min@zipfund.co>
2022-03-23 19:14:58 +0900

    fix : update MarkerClusterer to handle Marker and CustomOverlay as union

--- @types/MarkerClusterer.d.ts
+++ @types/MarkerClusterer.d.ts
@@ -19,7 +19,7 @@ declare namespace kakao.maps {
      * @param marker 추가할 마커
      * @param nodraw 클러스터 redraw 여부. true인 경우 클러스터를 다시 그리지 않는다.
      */
-    addMarker(marker: Marker, nodraw?: boolean): void;
+    addMarker(marker: Marker | CustomOverlay, nodraw?: boolean): void;
 
     /**
      * 클러스터에 추가된 마커 중 하나를 삭제한다.
@@ -27,7 +27,7 @@ declare namespace kakao.maps {
      * @param marker 삭제할 마커
      * @param nodraw 클러스터 redraw 여부. true인 경우 클러스터를 다시 그리지 않는다.
      */
-    removeMarker(marker: Marker, nodraw?: boolean): void;
+    removeMarker(marker: Marker | CustomOverlay, nodraw?: boolean): void;
 
     /**
      * 여러개의 마커를 추가한다.
@@ -35,7 +35,7 @@ declare namespace kakao.maps {
      * @param markers 추가할 마커 객체 배열
      * @param nodraw 클러스터 redraw 여부. true인 경우 클러스터를 다시 그리지 않는다.
      */
-    addMarkers(markers: Marker[], nodraw?: boolean): void;
+    addMarkers(markers: (Marker | CustomOverlay)[], nodraw?: boolean): void;
 
     /**
      * 추가된 마커 중 여러개의 마커를 삭제한다.
@@ -43,7 +43,7 @@ declare namespace kakao.maps {
      * @param markers 삭제할 마커 객체 배열
      * @param nodraw 클러스터 redraw 여부. true인 경우 클러스터를 다시 그리지 않는다.
      */
-    removeMarkers(markers: Marker[], nodraw?: boolean): void;
+    removeMarkers(markers: (Marker | CustomOverlay)[], nodraw?: boolean): void;
 
     /**
      * 추가된 모든 마커를 삭제한다.
@@ -172,7 +172,7 @@ declare namespace kakao.maps {
     /**
      * 클러스터링 할 마커 배열
      */
-    markers?: Marker[];
+    markers?: (Marker | CustomOverlay)[];
 
     /**
      * 클러스터의 격자 크기.
